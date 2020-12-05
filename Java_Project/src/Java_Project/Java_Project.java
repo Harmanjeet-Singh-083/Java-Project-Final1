8f36daab847fa823c7b06d7691743623c9d96964
Harmanjeet-083 <dell1@Dell>
2020-12-05 11:09:18 -0500

    I have created new class with the help of Sir

---
+++ Java_Project/src/Java_Project/Java_Project.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Java_Project;
+
+/**
+ * @author dell1
+ *
+ */
+public class Java_Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Harmanjeet Singh
+		System.out.println("Welcome to the java Project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
